edb46a3715cb2fcd203dff0524cddbd6734528f5
Mihai <mihai.babiac@gmail.com>
2016-03-25 13:00:07 +0200

    Renamed controller file and name

--- controllers/Hackathers.php
+++ controllers/Hackathers.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Lc extends CI_Controller {
+class Hackathers extends CI_Controller {
 
 	public function __construct()
 	{
